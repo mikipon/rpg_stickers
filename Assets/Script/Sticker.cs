9c548871a9bc4220a4fa88e83fe85b6f437a0a56
miki kimura <35397855+mikipon@users.noreply.github.com>
2021-05-13 01:46:53 +0900

    Initial commit

---
+++ Assets/Script/Sticker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public Sprite[] stickers;
+    public GameObject makeBox;
+
+    int count = 0;
+    bool gameStart;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Sticker")
+        {
+            count ++;
+
+            if (count == 20)
+            {
+                gameStart = true;
+            }
+
+        }
+    }
+
+    void Update()
+    {
+        if (gameStart)
+        {
+            int xRam = Random.Range(-9, 10); // 生成位置Xをランダム
+            int stiRam = Random.Range(0, stickers.Length); // 生成するステッカーをランダム
+
+            for (int i = 0; i < stickers.Length; i++)
+            {
+                GameObject temp = Instantiate(makeBox, new Vector2(xRam, 8), Quaternion.identity);
+                temp.GetComponent<SpriteRenderer>().sprite = stickers[stiRam];
+                temp.GetComponent<Rigidbody2D>().gravityScale = 2;
+            }
+        }    
+    }
+
+
+
+}
---
+++ Assets/Script/StickerManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickerManager : MonoBehaviour
+{
+
+    //public Sprite[] stickers;
+    public GameObject[] stickers;
+    //public GameObject makeBox;
+    public Transform parentTran;
+    public float height;
+
+    int count = 0;
+    bool m_gameStart;
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "sticker")
+        {
+            Debug.Log(count + ": 通った");
+
+            count++;
+
+            if (count > 10)
+            {
+                m_gameStart = true;
+            }
+            else
+            {
+                m_gameStart = false;
+            }
+
+        }
+    }
+
+    void Update()
+    {
+        //Debug.Log(m_gameStart.ToString());
+        if (m_gameStart)
+        {
+            //CreateObject();
+        }
+        else
+        {
+            //Debug.Log(m_gameStart.ToString());
+            CreateObject();
+        }
+    }
+
+    void CreateObject()
+    {
+        int ram = Random.Range(0, stickers.Length);
+        int xRam = Random.Range(-9, 10);
+
+        GameObject obj = Instantiate(stickers[ram], new Vector2(xRam, 8), Quaternion.identity);
+        //obj.transform.SetParent(parentTran);
+        //obj.transform.localPosition = new Vector2(0f, height);
+
+    }
+
+}
