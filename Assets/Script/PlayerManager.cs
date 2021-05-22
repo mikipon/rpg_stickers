f596c15dfdde9884e995d29f65316ff9f6a30423
miki kimura <35397855+mikipon@users.noreply.github.com>
2021-05-23 00:46:18 +0900

    Sticker制御とクリック制御追加

---
+++ Assets/Script/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    GameObject clickGameObject;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickGameObject = hit2d.transform.gameObject;
+            }
+
+            Debug.Log(clickGameObject.tag.ToString());
+
+            if (clickGameObject.tag == "sticker")
+            {
+                Debug.Log("その他のステッカー、何もなし");
+            }
+            else if (clickGameObject.tag == "weapon")
+            {
+                Debug.Log("武器ステッカー、攻撃");
+            }
+        }    
+    }
+}
--- Assets/Script/StickerManager.cs
+++ Assets/Script/StickerManager.cs
@@ -6,32 +6,35 @@ public class StickerManager : MonoBehaviour
 {
 
     //public Sprite[] stickers;
-    public GameObject[] stickers;
+    public GameObject[] weaponStickers;
+    public GameObject[] otherStickers;
+    public GameObject[] recoverySticker;
     //public GameObject makeBox;
     public Transform parentTran;
     public float height;
+    public int max = 30;
 
     int count = 0;
     bool m_gameStart;
-    bool m_displayComp;
+    //bool m_displayComp;
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.tag == "sticker")
+        if (other.tag == "sticker" || other.tag == "weapon")
         {
             //Debug.Log(count + ": 通った");
 
             count++;
 
-            if (count == 1)
+            if (count == max)
             {
-                m_displayComp = true;
+                //m_displayComp = true;
                 m_gameStart = true;
-                Debug.Log("10個でた");
+                Debug.Log(count + "全部でた");
             }
             else if(count < 1)
             {
-                m_displayComp = false;
+                //m_displayComp = false;
                 m_gameStart = false;
                 Debug.Log("それ以外");
             }
@@ -39,9 +42,14 @@ public class StickerManager : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        StartCoroutine("CreateCube");
+    }
+
     void Update()
     {
-        CreateObject(m_displayComp);
+        //StartCoroutine("CreateCube");
 
         if (m_gameStart) // ステッカーを表示し終えた
         {
@@ -49,15 +57,29 @@ public class StickerManager : MonoBehaviour
         }
     }
 
-    void CreateObject(bool bol)
+    /// <summary>
+    /// ステッカー生成
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator CreateCube()
     {
-        if (!bol) {
-                int ram = Random.Range(0, stickers.Length);
-                int xRam = Random.Range(-1, 1);
+        int w_max = 1;
+        int o_max = max - w_max;
+        for (int count = 0; count < w_max; count++) {
+            int ram = Random.Range(0, weaponStickers.Length);
+            int xRam = Random.Range(-6, 7);
 
-                GameObject obj = Instantiate(stickers[ram], new Vector2(xRam, 4), Quaternion.identity);
-                //obj.transform.SetParent(parentTran);
-                //obj.transform.localPosition = new Vector2(0f, height);]
+            yield return new WaitForSeconds(0.5f);
+            Instantiate(weaponStickers[ram], new Vector2(xRam, 6), Quaternion.identity);
+        }
+
+        for (int count = 0; count < o_max; count++)
+        {
+            int ram = Random.Range(0, weaponStickers.Length);
+            Random rnd = new Random();
+            int xRam = Random.Range(-6, 7);
+            yield return new WaitForSeconds(0.1f);
+            Instantiate(otherStickers[ram], new Vector2(xRam, 6), Quaternion.identity);
         }
     }
 
